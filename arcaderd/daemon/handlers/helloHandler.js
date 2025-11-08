5e333fb266e9aec6429f23a001fc6c678c04a64b
Mathias Wagner <germannewsmaker@gmail.com>
2025-11-08 23:04:37 +0100

    Remove logging from helloHandler.js

--- arcaderd/daemon/handlers/helloHandler.js
+++ arcaderd/daemon/handlers/helloHandler.js
@@ -2,9 +2,6 @@ import { sendResponse } from "../DaemonSocket.js";
 
 export const HELLO_MESSAGE_TYPE = "HELLO";
 
-export const handleSayHello = (socket, requestId, data) => {
-    const client = data?.client || "World";
-    console.log(`Got HELLO request from ${socket.remoteAddress} with name: ${client}`);
-
+export const handleSayHello = (socket, requestId) => {
     sendResponse(socket, { requestId, type: "HELLO_RESPONSE", success: true, data: undefined });
 };
